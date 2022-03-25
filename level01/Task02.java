b886d3dd5dd25411d934a688573daced939b9a5f
Ju-Han-kim <juhan_dev@naver.com>
2022-03-25 23:12:23 +0900

    프로그래머스 로또순위(완료)

---
+++ level01/Task02.java
@@ -0,0 +1,32 @@
+package level01;
+
+public class Task02 {
+	
+	public static void main(String[] args) {
+		
+		int[] lottos = {44, 1, 0, 0, 31, 25};
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		
+		int min = 7;
+		int cnt = 0;
+		
+		for(int i : lottos) {
+			if (i == 0){
+				cnt++;
+				continue;
+			}
+			for(int j : win_nums) {
+				if(i == j) {
+					min--;
+					break;
+				}
+			}
+		}
+		
+		min = min==7 ? 6 : min;
+		
+		System.out.println("max : "+ (min<cnt?1:min-cnt) + " / min : " + min);
+		
+	}
+	
+}
