e0b3e78d14832ab6cd235f049ca09e46a1e5064d
Ju-Han-kim <juhan_dev@naver.com>
2022-03-29 00:15:07 +0900

    level2 첫번째 문제 준비 완료

---
+++ level02/Task01.java
@@ -0,0 +1,27 @@
+package level02;
+
+/*
+ * 프로그래머스 level 02 단체사진 찍기
+ * 2017 카카오코드 본선
+ * 
+ */
+public class Task01 {
+
+	public static void main(String[] args) {
+		
+		Solution01 s = new Solution01();
+		
+		System.out.println();
+		
+	}
+
+}
+
+
+class Solution01 {
+	public int solution(int n, String[] data) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
