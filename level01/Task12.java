3debe44988af8a88bac20fae0899da53382837d6
Ju-Han-kim <juhan_dev@naver.com>
2022-03-28 23:56:51 +0900

    폰켓몬 문제 기본 셋팅

---
+++ level01/Task12.java
@@ -0,0 +1,25 @@
+package level01;
+
+
+/*
+ * 프로그래머스 level 01 폰켓몬
+ * 찾아라 프로그래밍 마에스터
+ */
+public class Task12 {
+
+	public static void main(String[] args) {
+		Solution11 s = new Solution11();
+		
+		System.out.println(s.solution(new int[] {3, 3, 3, 2, 2, 2}));
+	}
+
+}
+
+class Solution12 {
+	public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+    
+}
+
