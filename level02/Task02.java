6a3b534d76a1eafc404f78190b30b7d3d0195098
Ju-Han-kim <juhan_dev@naver.com>
2022-04-01 21:31:04 +0900

    컬러링북문제풀이 완료

--- level02/Task02.java
+++ level02/Task02.java
@@ -1,9 +1,6 @@
 package level02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 
 /*
  * 프로그래머스 level 02 카카오 프렌즈 컬러링북
@@ -14,60 +11,58 @@ public class Task02 {
 
 	public static void main(String[] args) {
 		Solution02 s = new Solution02();
-		
-		System.out.println(Arrays.toString(s.solution(6, 4, new int[][] {{1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}})));
+
+		System.out.println(Arrays.toString(s.solution(6, 4, new int[][] { { 1, 1, 1, 0 }, { 1, 1, 1, 0 },
+				{ 0, 0, 0, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 } })));
 	}
 
 }
 
-
 class Solution02 {
-    public int[] solution(int m, int n, int[][] picture) {
-        int[][] convertArea = new int[m][n];
-        
-        ArrayList<Integer> areaList = new ArrayList<Integer>();
-        
-        
-        /*
-         * 001
-         * 111
-         * 위 케이스에 에러 발생
-         */
-//        for(int row = 0; row < m; row++) {
-//        	for(int col = 0; col < n; col++) {
-//        		if(picture[row][col] == 0) continue;
-//        		
-//        		if(row != 0 && picture[row][col] == picture[row-1][col]){
-//        			convertArea[row][col] = convertArea[row-1][col];
-//        			areaList.set(convertArea[row-1][col], areaList.get(convertArea[row-1][col]) + 1);
-//        		} else if (col != 0 && picture[row][col] == picture[row][col-1]) {
-//        			
-//        			
-//        			
-//        			convertArea[row][col] = convertArea[row][col-1];
-//        			areaList.set(convertArea[row][col-1], areaList.get(convertArea[row][col-1]) + 1);
-//        		} else {
-//        			areaList.add(1);
-//        			convertArea[row][col] = areaList.size()-1;
-//        		}
-//        	}
-//        }
-        
-        
-        //
-        
-        System.out.println(areaList.toString());
-        
-        int[] answer = new int[2];
-        answer[0] = areaList.size();
-        answer[1] = Collections.max(areaList);
-        return answer;
-        
-        
-    }
-    boolean isEquals() {
-    	
-    	return true;
-    }
-    
+	int[][] pictureSnap;
+	boolean[][] chkPicture;
+	int max = 0;
+	int cnt = 0;
+
+	public int[] solution(int m, int n, int[][] picture) {
+
+		pictureSnap = picture;
+		chkPicture = new boolean[m][n];
+
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				if (pictureSnap[row][col] == 0 || chkPicture[row][col])
+					continue;
+				int tmpCnt = getArea(pictureSnap[row][col], row, col);
+				if (tmpCnt > 0) {
+					cnt++;
+					max = max > tmpCnt ? max : tmpCnt;
+				}
+			}
+		}
+
+		int[] answer = new int[2];
+		answer[0] = cnt;
+		answer[1] = max;
+		return answer;
+	}
+
+	public int getArea(int val, int row, int col) {
+		if (isEnd(row, col) || chkPicture[row][col])
+			return 0;
+
+		if (pictureSnap[row][col] != val)
+			return 0;
+		chkPicture[row][col] = true;
+
+		return 1 + getArea(pictureSnap[row][col], row - 1, col) + getArea(pictureSnap[row][col], row, col - 1)
+				+ getArea(pictureSnap[row][col], row + 1, col) + getArea(pictureSnap[row][col], row, col + 1);
+	}
+
+	public boolean isEnd(int row, int col) {
+		if (row >= chkPicture.length || row < 0 || col >= chkPicture[0].length || col < 0)
+			return true;
+		return false;
+	}
+
 }
