11635d28fa050ec5e28abb50eb7b3d2346e40e16
Ju-Han-kim <juhan_dev@naver.com>
2022-04-02 16:17:46 +0900

    기능개발 문제풀이 준비 완료

---
+++ level02/Task04.java
@@ -0,0 +1,27 @@
+package level02;
+
+import java.util.Arrays;
+
+/*
+ * 프로그래머스 level 02 기능개발
+ * 스택/큐
+ * 
+ */
+public class Task04 {
+
+	public static void main(String[] args) {
+		Solution04 s = new Solution04();
+
+		System.out.println(Arrays.toString(s.solution(new int[] {1,2,3}, new int[] {1,2,3})));
+	}
+
+}
+
+class Solution04 {
+	
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+    
+}
