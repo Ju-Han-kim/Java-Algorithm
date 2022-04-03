b19fde664a3a49a6378f11b9ef50d32faf594cfe
Ju-Han-kim <juhan_dev@naver.com>
2022-04-03 15:24:57 +0900

    문자열압축 문제풀이 준비 완료

---
+++ level02/Task05.java
@@ -0,0 +1,23 @@
+package level02;
+
+/*
+ * 프로그래머스 level 02 문자열 압축
+ * 2020 KAKAO BLIND RECRUITMENT
+ * 
+ */
+public class Task05 {
+
+	public static void main(String[] args) {
+		Solution05 s = new Solution05();
+
+		System.out.println(s.solution(""));
+	}
+
+}
+
+class Solution05 {
+	public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+}
