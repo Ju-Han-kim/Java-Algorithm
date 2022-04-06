96b293ca0b09c9274fa6a9616417c4ff38a44e4c
Ju-Han-kim <juhan_dev@naver.com>
2022-04-06 22:53:08 +0900

    멀쩡한 사각형 문제풀이 완료

--- level02/Task07.java
+++ level02/Task07.java
@@ -16,10 +16,17 @@ public class Task07 {
 }
 
 class Solution07 {
-	
     public long solution(int w, int h) {
-        long answer = 1;
+    	long gcd = gcd(w, h);
+        
+        // (전체 개수) - (꼭짓점 지나는 기준 사라지는 단위 개수) * 포함 수
+        long answer = ((long)w * h) - ( w/gcd + h/gcd - 1 ) * gcd;
+        
         return answer;
     }
     
+    public long gcd(long p, long q) {
+    	if (q == 0) return p;
+    	return gcd(q, p % q);
+    }
 }
