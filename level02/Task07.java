35788b856c6161622fedf7df8c96e99dc762b87a
Ju-Han-kim <juhan_dev@naver.com>
2022-04-06 22:11:09 +0900

    멀쩡한 사각형 문제풀이 준비

---
+++ level02/Task07.java
@@ -0,0 +1,25 @@
+package level02;
+
+/*
+ * 프로그래머스 level 02 멀쩡한 사각형
+ * Summer/Winter Coding(2019)
+ * 
+ */
+public class Task07 {
+
+	public static void main(String[] args) {
+		Solution07 s = new Solution07();
+		
+		System.out.println(s.solution(8, 12));
+	}
+
+}
+
+class Solution07 {
+	
+    public long solution(int w, int h) {
+        long answer = 1;
+        return answer;
+    }
+    
+}
